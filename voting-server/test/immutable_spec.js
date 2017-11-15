58d10bb54b2a3c4d8c23325e1985ab1a69e24510
Rohit Jose <rohitjose@gmail.com>
2017-11-16 07:12:08 +1100

    adds immutable test cases

---
+++ voting-server/test/immutable_spec.js
@@ -0,0 +1,38 @@
+import {expect} from 'chai'
+import {List} from 'immutable'
+
+describe('immutability', () => {
+
+	// Sample test case
+	describe('a number', () => {
+
+		function increment(currentState) {
+			return currentState + 1;
+		}
+
+		// Test Implementations
+		it('is immutable', () => {
+			let state = 68;
+			let nextState = increment(state);
+
+			expect(nextState).to.equal(69);
+			expect(state).to.equal(68);
+		});
+	});
+
+	// Immutable List
+	describe('a list', () => {
+
+		function addMovie(currentState, movie) {
+			return currentState.push(movie);
+		}
+
+		it('is immutable', () => {
+			let state = List.of('Trainspotting', '28 Days Later');
+			let nextState = addMovie(state, 'Sunshine');
+
+			expect(nextState).to.equal(List.of('Trainspotting', '28 Days Later', 'Sunshine'));
+			expect(state).to.equal(List.of('Trainspotting', '28 Days Later'));
+		});
+	});
+});
