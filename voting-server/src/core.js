95e4a9f0db7c65b995495f30d7f575b0655244f2
Rohit Jose <rohitjose@gmail.com>
2017-11-16 08:52:51 +1100

    adds next() function implementation and test cases

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -1,6 +1,18 @@
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 // Initializes the data structure
 export function setEntries(state, entries) {
 	return state.set('entries', List(entries));
+}
+
+// Generates the next state
+export function next(state) {
+	const entries = state.get('entries');
+
+	return state.merge({
+		vote: Map({
+			pair: entries.take(2)
+		}),
+		entries: entries.skip(2)
+	});
 }
