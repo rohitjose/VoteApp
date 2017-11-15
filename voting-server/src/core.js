915b043b489752c0420f1643e3e8457f7c1f86c3
Rohit Jose <rohitjose@gmail.com>
2017-11-16 09:23:02 +1100

    adds vote() function implementation and unit tests

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -16,3 +16,11 @@ export function next(state) {
 		entries: entries.skip(2)
 	});
 }
+
+// Generates vote count based on votes
+export function vote(state, entry){
+	return state.updateIn(
+		['vote','tally', entry],
+		0,
+		tally => tally + 1);
+}
