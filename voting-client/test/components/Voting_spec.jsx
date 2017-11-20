a65f9846416647ae9fe36271e1fbacce7d16c43f
Rohit Jose <rohitjose@gmail.com>
2017-11-21 08:19:55 +1100

    adds unit test for click event on the voting screen

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { renderIntoDocument, scryRenderedDOMComponentsWithTag } from 'react-addons-test-utils';
-import {expect} from 'chai';
+import { renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate } from 'react-addons-test-utils';
+import { expect } from 'chai';
 
 import Voting from '../../src/components/Voting';
 
@@ -16,5 +16,19 @@ describe('Voting', () => {
         expect(buttons.length).to.equal(2);
         expect(buttons[0].textContent).to.equal('Trainspotting');
         expect(buttons[1].textContent).to.equal('28 Days Later');
+    });
+
+    it('invokes callback when a button is clicked', () => {
+        let voteWith;
+        const vote = (entry) => voteWith = entry;
+
+        const component = renderIntoDocument(
+            <Voting pair={["Trainspotting", "Fight Club"]} vote={vote} />
+        );
+
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+        Simulate.click(buttons[0]);
+
+        expect(voteWith).to.equal("Trainspotting");
     });
 });
